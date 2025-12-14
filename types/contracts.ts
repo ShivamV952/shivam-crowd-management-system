91758d795ec9834edb403da10b53f0c2e041ddaf
ShivamV952 <sv2923976@gmail.com>
2025-12-14 17:48:34 +0530

    site dropdown api integrated

--- types/contracts.ts
+++ types/contracts.ts
@@ -7,3 +7,20 @@ export interface LoginResponse {
   token: string;
 }
 
+export interface Zone {
+  zoneId: string;
+  name: string;
+  securityLevel: string;
+}
+
+export interface Site {
+  siteId: string;
+  name: string;
+  timezone: string;
+  country: string;
+  city: string;
+  zones: Zone[];
+}
+
+export type SitesResponse = Site[];
+
